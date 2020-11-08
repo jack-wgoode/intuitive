e5655bfa6e3ae28e895511d01aaa6ef4a8966636
jack-wgoode <jack.w.goode@icloud.com>
2020-11-08 18:01:04 -0500

    edited tech controller, index, create, whitelist

--- app/controllers/techs_controller.rb
+++ app/controllers/techs_controller.rb
@@ -1,2 +1,25 @@
 class TechsController < ApplicationController
+
+    def index
+        @techs = Tech.all
+    end
+
+    def new
+        @tech = Tech.new
+    end
+
+    def create
+        @tech = Tech.new(tech_params)  //think this through- how is it associated with the end_user and ticket?
+        if @tech.save
+            //show page
+        else
+            :new
+    end
+
+
+
+
+        def tech_params
+            permitted = params.require(:name, :department, :cert_level, :email).permit(:name, :department, :cert_level, :email)
+        end
 end
